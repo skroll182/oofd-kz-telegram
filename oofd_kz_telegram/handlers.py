7dea0db668353079532c5178274d7551a72036fd
Kirill Olar <kirill.olar26@gmail.com>
2021-11-15 10:36:27 +0600

    updated oofd_kz_parser

--- oofd_kz_telegram/handlers.py
+++ oofd_kz_telegram/handlers.py
@@ -3,7 +3,8 @@ import pathlib
 from tempfile import NamedTemporaryFile
 
 from aiogram.types import KeyboardButton, Message, ReplyKeyboardMarkup
-from oofd_kz_parser.parser import TicketParser
+from oofd_kz_parser.exceptions import QRNotFoundException
+from oofd_kz_parser.parser import parse_from_qr
 from oofd_kz_telegram.states import States
 from PIL import Image
 
@@ -50,7 +51,7 @@ async def handle_qr_photo(message: Message):
     await message.photo[-1].download(destination_file=photo_file_name)
     img = Image.open(photo_file_name)
     try:
-        ticket = TicketParser.from_qr(img)
+        ticket = parse_from_qr(img)
         ticket_text = ticket.json(indent=2)
         message_chunks = [
             ticket_text[i * 4096 : (i * 4096) + 4096] for i in range(round(len(ticket_text) / 4096))
@@ -58,7 +59,7 @@ async def handle_qr_photo(message: Message):
         for message_chunk in message_chunks:
             await message.bot.send_message(chat_id=message.chat.id, text=message_chunk)
         await handle_start(message)
-    except ValueError:
+    except QRNotFoundException:
         await message.bot.send_message(chat_id=message.chat.id, text="Не удалось распознать QR код")
         await handle_add_ticket_by_qr(message)
     finally:
